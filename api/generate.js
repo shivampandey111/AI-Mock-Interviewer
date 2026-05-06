f65ad7a53f1a52c0276d476e4cdee4b8e721c010
Shivam Pandey <shivampandey33033@gmail.com>
2026-05-06 11:10:27 +0530

    Improved readability of generate.js

--- api/generate.js
+++ api/generate.js
@@ -24,7 +24,6 @@ export default async function handler(req, res) {
         res.status(200).json(data);
 
     } catch (error) {
-        console.error("ERROR:", error);
         res.status(500).json({ error: "API request failed" });
     }
 }
